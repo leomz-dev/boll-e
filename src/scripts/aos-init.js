083f2f19704a665bf66be14f78c72f679e717613
Leonardo Meza <168940126+leomz-dev@users.noreply.github.com>
2025-09-17 09:43:33 -0500

    Fix AOS initialization error

--- src/scripts/aos-init.js
+++ src/scripts/aos-init.js
@@ -1,6 +1,8 @@
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
-AOS.init({
-  // Puedes configurar opciones de AOS aquí si lo deseas
+document.addEventListener('DOMContentLoaded', function() {
+  AOS.init({
+    // Puedes configurar opciones de AOS aquí si lo deseas
+  });
 });
