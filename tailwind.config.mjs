b178b65ea83e2625cd02b9049d829749f6a4972d
Leonardo Meza <168940126+leomz-dev@users.noreply.github.com>
2025-09-17 09:09:06 -0500

    Completar secciones del portafolio BPO

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,7 +6,7 @@ export default {
       colors: {
         'brand-background': '#c9c2b2', // Example background color
         'brand-text': '#2C3E50', // Example text color
-        'brand-primary': '#4A90E2', // Example primary color for hover states
+        'brand-primary': '#4A90E2', // Primary color for buttons and accents
         'brand-secondary': '#50E3C2', // Example secondary color
       },
       fontFamily: {
